4e94fc8f3ae309b98750878380e7e91925af5b0f
Bo Anderson <2323889A@student.gla.ac.uk>
2018-03-09 13:19:44 +0000

    Initial work on models.

--- tragicreviews/admin.py
+++ tragicreviews/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from tragicreviews.models import Category, Article, Rating, Comment
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+	prepopulated_fields = {"slug": ("name",)}
+admin.site.register(Category, CategoryAdmin)
+
+class ArticleAdmin(admin.ModelAdmin):
+	list_display = ("title", "body", "author")
+admin.site.register(Article, ArticleAdmin)
+
+admin.site.register(Rating)
+admin.site.register(Comment)
--- tragicreviews/models.py
+++ tragicreviews/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=32, unique=True)
+	slug = models.SlugField(max_length=32, primary_key=True)
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Category, self).save(*args, **kwargs)
+
+	class Meta:
+		verbose_name_plural = "Categories"
+
+	def __str__(self):
+		return self.name
+
+class Article(models.Model):
+	category = models.ForeignKey(Category)
+	title = models.CharField(max_length=128)
+	body = models.TextField(max_length=5000)
+	author = models.ForeignKey(User)
+
+class Rating(models.Model):
+	article = models.ForeignKey(Article)
+	user = models.ForeignKey(User)
+	rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+	class Meta:
+		unique_together = ('article', 'user')
+
+class Comment(models.Model):
+	article = models.ForeignKey(Article)
+	user = models.ForeignKey(User)
+	text = models.TextField(max_length=500)
