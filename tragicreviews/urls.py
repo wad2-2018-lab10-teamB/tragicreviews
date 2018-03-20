73ebd02d9fb0bb80e9db5816a0059331ec16f1d2
a-joyce <alexajoyce4@gmail.com>
2018-03-20 22:18:11 +0000

    Fixed URLs (though add_article chooses to be weird every now and then) and done views stuff- will crack on tonight but here’s what i have so far so you can get on Finn! Sry for delay

--- tragicreviews/urls.py
+++ tragicreviews/urls.py
@@ -2,22 +2,20 @@ from django.conf.urls import url
 from tragicreviews import views
 
 urlpatterns = [
-    #url(r'^login/$', views.user_login, name='login'),
-    # url(r'^restricted/$', views.restricted, name='restricted'),
-    #url(r'^logout/$', views.user_logout, name='logout'),
-    #url(r'^registration_form/$', views.registration_form, name='registration_form'),
-    #url(r'^registration_complete/$', views.registration_complete, name='registration_complete'),
-	url(r'^article/$', views.about, name='article'),
-	url(r'^category/(?P<category_name_url>\w+)/$', views.category, name='category'),
-	url(r'^add_article/$', views.add_article, name='add_article'),
-	url(r'^category/(?P<category_name_url>\w+)/add_article/$', views.add_page, name='add_page'),
-    url(r'^$', views.index, name='index'),
-	#url(r'^search/$', views.search, name='search'),
+    url(r'^add_article/$', views.add_article, name='add_article'),
+    url(r'^article/$', views.article, name='article'),
+    url(r'^category/(?P<category_name_url>\w+)/$', views.category, name='category'),
+    # url(r'^/tragicreviews/$', views.index, name='index'),
     url(r'^profile/$', views.profile, name='profile'),
     url(r'^profile_reviews/$', views.profile_reviews, name='profile_reviews'),
     url(r'^profile_uploads/$', views.profile_uploads, name='profile_uploads'),
-    url(r'^goto/$', views.track_url, name='track_url'),
+    url(r'^$', views.index, name='index'),
+    # url(r'^restricted/$', views.restricted, name='restricted'),
+    # url(r'^category/(?P<category_name_url>\w+)/add_article/$', views.add_page, name='add_page'),
+    # #url(r'^search/$', views.search, name='search'),
+    # url(r'^goto/$', views.track_url, name='track_url'),
     # url(r'^like_category/$', views.like_category, name='like_category'),
     # url(r'^suggest_category/$', views.suggest_category, name='suggest_category'),
     # url(r'^auto_add_page/$', views.auto_add_page, name='auto_add_page'),
-]
+	]
+
--- tragicreviews/views.py
+++ tragicreviews/views.py
@@ -1,439 +1,125 @@
-from django.template import RequestContext
-from django.shortcuts import render_to_response
-from django.http import HttpResponse
-from tragicreviews.models import Category
-from tragicreviews.models import Page
-from tragicreviews.models import UserProfile
-from tragicreviews.forms import UserForm, UserProfileForm
-from tragicreviews.forms import CategoryForm, PageForm
-from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponseRedirect
+# from django.http import HttpResponse, HttpResponseRedirect, Http404
+# from django.urls import reverse
+from django.shortcuts import redirect, render
+# from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.models import User
+# from django.bing_search import run_query
+# from tragicreviews.template import RequestContext
+from tragicreviews.models import Subject, UserProfile, UserLevelField, UserProfileManager, Article, Rating, Comment, ArticleViews
+from tragicreviews.forms import UserRegistrationForm, SubjectForm, ArticleForm, CommentForm
 from datetime import datetime
-from tragicreviews.bing_search import run_query
-from django.contrib.auth.models import User
-from django.shortcuts import redirect
+from tragicreviews.helperFunctions import *
 
 def encode_url(str):
     return str.replace(' ', '_')
-
-
 def decode_url(str):
     return str.replace('_', ' ')
 
+@login_required
+def add_article(request):
 
-def get_category_list(max_results=0, starts_with=''):
-    cat_list = []
-    if starts_with:
-        cat_list = Category.objects.filter(name__startswith=starts_with)
+    context_dict = {'categories': []}
+    subjects = Subject.objects.all()
+
+    for subject in subjects:
+        context_dict['categories'].append(subject)
+
+    return render(request, '/tragicreviews/add_article.html', context_dict)
+
+
+def article(request, req_title):
+    context_dict = {'title': [], 'author':"", 'text':"", }
+    # 'comment_author': "", 'comment_date':"", 'comment_author_pic':""
+
+    article_object = Article.objects.filter(title = req_title)
+    if article_object.exists():
+        context_dict['title'] = article_object.title
+        context_dict['author'] = article_object.author
+        context_dict['text'] = article_object.body
+        # according to bo :P
+        context_dict.update('comment_set':[Comment.objects.get(article = article_object)])
     else:
-        cat_list = Category.objects.all()
+        return False
 
-    if max_results > 0:
-        if (len(cat_list) > max_results):
-            cat_list = cat_list[:max_results]
+    return render(request, 'tragicreviews/article.html', context_dict)
 
-    for cat in cat_list:
-        cat.url = encode_url(cat.name)
 
-    return cat_list
+def base(request):
+    context_dict = {}
+    return render(request, 'tragicreviews/base.html', context_dict)
+
+
+def base_bootstrap(request):
+    context_dict = {'categories': []}
+
+    subjects = Subject.name.all()
+
+    for subject in subjects:
+        context_dict['categories'].append(subject)
+
+
+    return render(request, 'tragicreviews/base_bootstrap.html', context_dict)
+
+
+def category(request, req_cat):
+    #Needs a title, author and preview
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        articles = Article.objects.filter(category=category)
+
+        context_dict['articles'] = articles
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'tragicreviews/category.html', context_dict)
 
 
 def index(request):
-    context = RequestContext(request)
 
-    top_category_list = Category.objects.order_by('-views')[:5]
+    trend_cat_list = ArticleViews.objects.get_trending_articles(limit=5, days=14)
 
-    for category in top_category_list:
-        category.url = encode_url(category.name)
+    for cat in trend_cat_list:
+        cat.url = encode_url(cat.name)
 
-    context_dict = {'categories': top_category_list}
+    context_dict = {'categories': trend_cat_list}
 
-    cat_list = get_category_list()
-    context_dict['cat_list'] = cat_list
+    return render(request, 'tragicreviews/index.html', context_dict)
 
-    page_list = Page.objects.order_by('-views')[:5]
-    context_dict['pages'] = page_list
 
-    if request.session.get('last_visit'):
-        # The session has a value for the last visit
-        last_visit_time = request.session.get('last_visit')
-
-        visits = request.session.get('visits', 0)
-
-        if (datetime.now() - datetime.strptime(last_visit_time[:-7], "%Y-%m-%d %H:%M:%S")).days > 0:
-            request.session['visits'] = visits + 1
-    else:
-        # The get returns None, and the session does not have a value for the last visit.
-        request.session['last_visit'] = str(datetime.now())
-        request.session['visits'] = 1
-
-    # Render and return the rendered response back to the user.
-    return render_to_response('tragicreviews/index.html', context_dict, context)
-
-
-def about(request):
-    # Request the context.
-    context = RequestContext(request)
-    context_dict = {}
-    cat_list = get_category_list()
-    context_dict['cat_list'] = cat_list
-    # If the visits session varible exists, take it and use it.
-    # If it doesn't, we haven't visited the site so set the count to zero.
-
-    count = request.session.get('visits', 0)
-
-    context_dict['visit_count'] = count
-
-    # Return and render the response, ensuring the count is passed to the template engine.
-    return render_to_response('tragicreviews/about.html', context_dict, context)
-
-
-def category(request, category_name_url):
-    # Request our context
-    context = RequestContext(request)
-
-    # Change underscores in the category name to spaces.
-    # URL's don't handle spaces well, so we encode them as underscores.
-    category_name = decode_url(category_name_url)
-
-    # Build up the dictionary we will use as out template context dictionary.
-    context_dict = {'category_name': category_name, 'category_name_url': category_name_url}
-
-    cat_list = get_category_list()
-    context_dict['cat_list'] = cat_list
-
-    try:
-        # Find the category with the given name.
-        # Raises an exception if the category doesn't exist.
-        # We also do a case insensitive match.
-        category = Category.objects.get(name__iexact=category_name)
-        context_dict['category'] = category
-        # Retrieve all the associated pages.
-        # Note that filter returns >= 1 model instance.
-        pages = Page.objects.filter(category=category).order_by('-views')
-
-        # Adds our results list to the template context under name pages.
-        context_dict['pages'] = pages
-    except Category.DoesNotExist:
-        # We get here if the category does not exist.
-        # Will trigger the template to display the 'no category' message.
-        pass
-
-    if request.method == 'POST':
-        query = request.POST.get('query')
-        if query:
-            query = query.strip()
-            result_list = run_query(query)
-            context_dict['result_list'] = result_list
-
-    # Go render the response and return it to the client.
-    return render_to_response('tragicreviews/category.html', context_dict, context)
-
-
-@login_required
-def add_category(request):
-    # Get the context from the request.
-    context = RequestContext(request)
-    cat_list = get_category_list()
-    context_dict = {}
-    context_dict['cat_list'] = cat_list
-
-    # A HTTP POST?
-    if request.method == 'POST':
-        form = CategoryForm(request.POST)
-
-        # Have we been provided with a valid form?
-        if form.is_valid():
-            # Save the new category to the database.
-            form.save(commit=True)
-
-            # Now call the index() view.
-            # The user will be shown the homepage.
-            return index(request)
-        else:
-            # The supplied form contained errors - just print them to the terminal.
-            print(form.errors)
-    else:
-        # If the request was not a POST, display the form to enter details.
-        form = CategoryForm()
-
-    # Bad form (or form details), no form supplied...
-    # Render the form with error messages (if any).
-    context_dict['form'] = form
-    return render_to_response('tragicreviews/add_category.html', context_dict, context)
-
-
-# @login_required
-# def add_page(request, category_name_url):
-#     context = RequestContext(request)
-#     cat_list = get_category_list()
-#     context_dict = {}
-#     context_dict['cat_list'] = cat_list
-#
-#     category_name = decode_url(category_name_url)
-#     if request.method == 'POST':
-#         form = PageForm(request.POST)
-#
-#         if form.is_valid():
-#             # This time we cannot commit straight away.
-#             # Not all fields are automatically populated!
-#             page = form.save(commit=False)
-#
-#             # Retrieve the associated Category object so we can add it.
-#             try:
-#                 cat = Category.objects.get(name=category_name)
-#                 page.category = cat
-#             except Category.DoesNotExist:
-#                 return render_to_response('tragicreviews/add_page.html',
-#                                           context_dict,
-#                                           context)
-#
-#             # Also, create a default value for the number of views.
-#             page.views = 0
-#
-#             # With this, we can then save our new model instance.
-#             page.save()
-#
-#             # Now that the page is saved, display the category instead.
-#             return category(request, category_name_url)
-#         else:
-#             print(form.errors)
-#     else:
-#         form = PageForm()
-#
-#     context_dict['category_name_url'] = category_name_url
-#     context_dict['category_name'] = category_name
-#     context_dict['form'] = form
-#
-#     return render_to_response('tragicreviews/add_page.html',
-#                               context_dict,
-#                               context)
-
-
-def registration_(request):
-    # Request the context.
-    context = RequestContext(request)
-    cat_list = get_category_list()
-    context_dict = {}
-    context_dict['cat_list'] = cat_list
-    # Boolean telling us whether registration was successful or not.
-    # Initially False; presume it was a failure until proven otherwise!
-    registered = False
-
-    # If HTTP POST, we wish to process form data and create an account.
-    if request.method == 'POST':
-        # Grab raw form data - making use of both FormModels.
-        user_form = UserForm(data=request.POST)
-        profile_form = UserProfileForm(data=request.POST)
-
-        # Two valid forms?
-        if user_form.is_valid() and profile_form.is_valid():
-            # Save the user's form data. That one is easy.
-            user = user_form.save()
-
-            # Now a user account exists, we hash the password with the set_password() method.
-            # Then we can update the account with .save().
-            user.set_password(user.password)
-            user.save()
-
-            # Now we can sort out the UserProfile instance.
-            # We'll be setting values for the instance ourselves, so commit=False prevents Django from saving the instance automatically.
-            profile = profile_form.save(commit=False)
-            profile.user = user
-
-            # Profile picture supplied? If so, we put it in the new UserProfile.
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
-
-            # Now we save the model instance!
-            profile.save()
-
-            # We can say registration was successful.
-            registered = True
-
-        # Invalid form(s) - just print errors to the terminal.
-        else:
-            print(user_form.errors, profile_form.errors)
-
-    # Not a HTTP POST, so we render the two ModelForms to allow a user to input their data.
-    else:
-        user_form = UserForm()
-        profile_form = UserProfileForm()
-
-    context_dict['user_form'] = user_form
-    context_dict['profile_form'] = profile_form
-    context_dict['registered'] = registered
-
-    # Render and return!
-    return render_to_response(
-        'tragicreviews/register.html',
-        context_dict,
-        context)
-
-
-def user_login(request):
-    # Obtain our request's context.
-    context = RequestContext(request)
-    cat_list = get_category_list()
-    context_dict = {}
-    context_dict['cat_list'] = cat_list
-
-    # If HTTP POST, pull out form data and process it.
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-
-        # Attempt to log the user in with the supplied credentials.
-        # A User object is returned if correct - None if not.
-        user = authenticate(username=username, password=password)
-
-        # A valid user logged in?
-        if user is not None:
-            # Check if the account is active (can be used).
-            # If so, log the user in and redirect them to the homepage.
-            if user.is_active:
-                login(request, user)
-                return HttpResponseRedirect('/tragicreviews/')
-            # The account is inactive; tell by adding variable to the template context.
-            else:
-                context_dict['disabled_account'] = True
-                return render_to_response('tragicreviews/login.html', context_dict, context)
-        # Invalid login details supplied!
-        else:
-            print("Invalid login details: {0}, {1}".format(username, password))
-            context_dict['bad_details'] = True
-            return render_to_response('tragicreviews/login.html', context_dict, context)
-
-    # Not a HTTP POST - most likely a HTTP GET. In this case, we render the login form for the user.
-    else:
-        return render_to_response('tragicreviews/login.html', context_dict, context)
-
-
-@login_required
-def restricted(request):
-    context = RequestContext(request)
-    cat_list = get_category_list()
-    context_dict = {}
-    context_dict['cat_list'] = cat_list
-    return render_to_response('tragicreviews/restricted.html', context_dict, context)
-
-
-# Only allow logged in users to logout - add the @login_required decorator!
-@login_required
-def user_logout(request):
-    # As we can assume the user is logged in, we can just log them out.
-    logout(request)
-
-    # Take the user back to the homepage.
-    return HttpResponseRedirect('/tragicreviews/')
-
-
-def search(request):
-    context = RequestContext(request)
-
-    cat_list = get_category_list()
-    context_dict = {}
-    context_dict['cat_list'] = cat_list
-
-    result_list = []
-
-    if request.method == 'POST':
-        query = request.POST['query'].strip()
-
-        if query:
-            # Run our Bing function to get the results list!
-            result_list = run_query(query)
-
-    context_dict['result_list'] = result_list
-    return render_to_response('tragicreviews/search.html', context_dict, context)
-
-
-@login_required
 def profile(request):
-    context = RequestContext(request)
-    cat_list = get_category_list()
-    context_dict = {'cat_list': cat_list}
-    u = User.objects.get(username=request.user)
+    user_dictionary = {}
 
-    try:
-        up = UserProfile.objects.get(user=u)
-    except:
-        up = None
+    # Map a username to all the user's details
+    user_dictionary[UserProfile.user.username] = {
+        'user': UserProfile.user.username,
+        'image': UserProfile.image if bool(UserProfile.image) else False,
+        'levels': UserProfile.level,
+        'majors': UserProfile.majors.all(),
+    }
 
-    context_dict['user'] = u
-    context_dict['userprofile'] = up
-    return render_to_response('tragicreviews/profile.html', context_dict, context)
+    context_dict = {'userProfile': {}}
+    loggedUser = UserProfile.objects.get(user=request.user)
 
+    context_dict['userProfile'].update(getUserDetails(loggedUser))
 
-def track_url(request):
-    context = RequestContext(request)
-    page_id = None
-    url = '/tragicreviews/'
-    if request.method == 'GET':
-        if 'page_id' in request.GET:
-            page_id = request.GET['page_id']
-            try:
-                page = Page.objects.get(id=page_id)
-                page.views = page.views + 1
-                page.save()
-                url = page.url
-            except:
-                pass
-
-    return redirect(url)
+    return render(request, 'tragicreviews/profile.html', context_dict)
 
 
-@login_required
-def like_category(request):
-    context = RequestContext(request)
-    cat_id = None
-    if request.method == 'GET':
-        cat_id = request.GET['category_id']
-
-    likes = 0
-    if cat_id:
-        category = Category.objects.get(id=int(cat_id))
-        if category:
-            likes = category.likes + 1
-            category.likes = likes
-            category.save()
-
-    return HttpResponse(likes)
-
-
-def suggest_category(request):
-    context = RequestContext(request)
-    cat_list = []
-    starts_with = ''
-    if request.method == 'GET':
-        starts_with = request.GET['suggestion']
-    else:
-        starts_with = request.POST['suggestion']
-
-    cat_list = get_category_list(8, starts_with)
-
-    return render_to_response('tragicreviews/category_list.html', {'cat_list': cat_list}, context)
-
-
-@login_required
-def auto_add_page(request):
-    context = RequestContext(request)
-    cat_id = None
-    url = None
-    title = None
+def profile_reviews(request):
     context_dict = {}
-    if request.method == 'GET':
-        cat_id = request.GET['category_id']
-        url = request.GET['url']
-        title = request.GET['title']
-        if cat_id:
-            category = Category.objects.get(id=int(cat_id))
-            p = Page.objects.get_or_create(category=category, title=title, url=url)
 
-            pages = Page.objects.filter(category=category).order_by('-views')
 
-            # Adds our results list to the template context under name pages.
-            context_dict['pages'] = pages
+    return render(request, 'tragicreviews/profile_reviews.html', context_dict)
 
-    return render_to_response('tragicreviews/page_list.html', context_dict, context)
+@login_required
+def profile_uploads(request):
+    context_dict = {}
+    return render(request, 'tragicreviews/profile_uploads.html', context_dict)
+
+
