1cbe23a918e798132ec07cc81694580b3a4d8841
Xueying Qin <2335466q@student.gla.ac.uk>
2018-03-20 23:01:50 +0000

    Sorry for another typo..

--- tragicreviews_project/urls.py
+++ tragicreviews_project/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^tragicreviews/$', include('tragicreviews.urls')),
+    url(r'^tragicreviews/', include('tragicreviews.urls')),
 ]
 
 
