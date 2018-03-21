98be75790c78b0d7079cf70ffaa59fb26dcc519c
Xueying Qin <2335466q@student.gla.ac.uk>
2018-03-21 17:08:18 +0000

    Fix a bug of disabled select value

--- static/js/level.js
+++ static/js/level.js
@@ -2,10 +2,12 @@ document.getElementById("id_staff_level").disabled = true;
 document.getElementById("id_student_level").disabled = true;
 document.getElementById("id_group").onchange = function(){
     if (document.getElementById("id_group").value === "1"){
+        document.getElementById("id_staff_level").value = "";
         document.getElementById("id_staff_level").disabled = true;
         document.getElementById("id_student_level").disabled = false;
     } else if (document.getElementById("id_group").value === "2"){
         document.getElementById("id_staff_level").disabled = false;
+        document.getElementById("id_student_level").value = "";
         document.getElementById("id_student_level").disabled = true;
     } else {
         alert("No input >.<");
