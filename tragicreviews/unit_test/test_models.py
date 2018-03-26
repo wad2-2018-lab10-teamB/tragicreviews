dafd59c293fdedc77a437d71dbcb1439c29e2dd2
Xueying Qin <2335466q@student.gla.ac.uk>
2018-03-26 18:15:34 +0100

    Update test cases

--- tragicreviews/unit_test/test_models.py
+++ tragicreviews/unit_test/test_models.py
@@ -4,17 +4,17 @@ from django.contrib.auth.models import User, Group
 from django.core.exceptions import ValidationError
 import tragicreviews.unit_test.test_utils as test_utils
 from datetime import datetime
-
 from django.db.utils import IntegrityError
 from django.db import transaction
-
 from freezegun import freeze_time
+from io import StringIO
+import sys
 
 
 # Testing Models
 class ModelTests(TestCase):
 
-    def test_creating_category(self):
+    def test_create_category(self):
         s = Subject(name="Computing Science")
         s.save()
         subjects = Subject.objects.all()
@@ -25,34 +25,75 @@ class ModelTests(TestCase):
         # Test slug is correct
         self.assertEqual(subjects[0].slug, "computing-science")
 
-    def test_creating_user_profile(self):
+    def test_create_user_profile(self):
         user_pf = test_utils.create_user()
         UserProfile.objects.create_user("mummy")
         self.assertEqual(User.objects.all().count(), 2)
         self.assertEqual(UserProfile.objects.all().count(), 2)
         self.assertEqual(UserProfile.objects.all()[0], user_pf)
 
-        # Test Integrity
-        UserProfile.objects.all().delete()
+        # Test one to one field on deletion
+        # Delete a user, the user profile will also be deleted
         User.objects.all().delete()
+        self.assertEqual(UserProfile.objects.all().count(), 0)
+
+        # Delete a user profile, the user will also be deleted
+        UserProfile.objects.create_user("mummy")
+        UserProfile.objects.all().delete()
+        self.assertEqual(User.objects.all().count(), 0)
+
+        # Test Integrity
+        user_pf1 = UserProfile.objects.create_user("mummy")
+        user_pf2 = UserProfile(user=user_pf1.user)
+        self.assertRaises(ValidationError, lambda: user_pf2.save())
+
+    def test_get_user_profile_by_username(self):
         user_pf1 = UserProfile.objects.create_user("mummy")
         user_pf2 = UserProfile.objects.create_user("dummy")
-        user_pf3 = UserProfile(user=user_pf1.user)
-        self.assertRaises(ValidationError, lambda: user_pf3.save())
 
-        # Something weird, will fix it later
-        '''
-        user_pf2.user = user_pf1.user
+        # Test getting correct user according to username
+        self.assertEqual(user_pf1, UserProfile.objects.get_by_username("mummy"))
+        self.assertNotEqual(user_pf2, UserProfile.objects.get_by_username("mummy"))
+
+    def test_user_profile_group(self):
+        test_utils.create_groups()
+        user_pf1 = UserProfile.objects.create_user("mummy")
+        user_pf2 = UserProfile.objects.create_user("dummy")
+        user_pf1.user.groups.add(Group.objects.get(name="staff"))
+        user_pf2.user.groups.add(Group.objects.get(name="student"))
+        user_pf1.save()
         user_pf2.save()
-        user_pf2.user.save()
-        print(user_pf1.user)
-        print(user_pf2.user)
-        print(user_pf1)
-        print(user_pf2)
-        print(UserProfile.objects.all())
-        '''
 
-    def test_creating_article(self):
+        # Test checking user group
+        self.assertTrue(UserProfile.is_member(user_pf1, "staff"))
+        self.assertFalse(UserProfile.is_member(user_pf1, "student"))
+        self.assertTrue(UserProfile.is_member(user_pf2, "student"))
+        self.assertFalse(UserProfile.is_member(user_pf2, "staff"))
+
+        # Test getting correct names of roles
+        self.assertEqual(UserProfile.get_role(user_pf1), "Staff")
+        self.assertEqual(UserProfile.get_role(user_pf2), "Student")
+
+    def test_user_profile_name_representation(self):
+        test_utils.create_groups()
+        user_pf1 = UserProfile.objects.create_user("mummy")
+        user_pf2 = UserProfile.objects.create_user("dummy")
+        user_pf1.user.groups.add(Group.objects.get(name="staff"))
+        user_pf2.user.groups.add(Group.objects.get(name="student"))
+        user_pf1.save()
+        user_pf2.save()
+
+        # Test lecture user profile
+        self.held, sys.stdout = sys.stdout, StringIO()
+        print(user_pf1)
+        self.assertEqual("mummy ✔", sys.stdout.getvalue().strip())
+
+        # Test student user profile
+        self.held, sys.stdout = sys.stdout, StringIO()
+        print(user_pf2)
+        self.assertEqual("dummy", sys.stdout.getvalue().strip())
+
+    def test_create_article(self):
         user_pf = test_utils.create_user()
         s = Subject(name="Computing Science")
         s.save()
@@ -92,7 +133,7 @@ class ModelTests(TestCase):
         zero_val = 0
         large_val = 6
 
-        # test creating rating
+        # Test creating rating
         rating0 = Rating(user=user, article=article, rating=3)
         rating0.save()
         self.assertEqual(Rating.objects.all().count(), 1)
@@ -102,26 +143,41 @@ class ModelTests(TestCase):
 
         rating1 = Rating(user=user, article=article, rating=4)
 
-        # test integrity
+        # Test integrity
         self.assertRaises(ValidationError, lambda: rating1.save())
 
-        # test same user could rate different articles and same article could be rated by different users
+        # Test same user could rate different articles and same article could be rated by different users
         rating2 = Rating(user=user2, article=article, rating=5)
         rating3 = Rating(user=user, article=article2, rating=5)
         self.assertIsNone(rating2.save())
         self.assertIsNone(rating3.save())
         self.assertEqual(Rating.objects.all().count(), 3)
-        Rating.objects.all().delete()
 
-        # test a rating with negative value
+        # Test the number of ratings created by a user is correct
+        self.assertEqual(user.rating_set.count(), 2)
+
+        # Test when ratings are deleted, they are also deleted from a user's rating set
+        Rating.objects.all().delete()
+        self.assertEqual(user.rating_set.count(), 0)
+
+        # Test when a user is deleted, the user's ratings are all also deleted
+        rating0 = Rating(user=user2, article=article, rating=3)
+        rating0.save()
+        rating1 = Rating(user=user2, article=article2, rating=5)
+        rating1.save()
+        self.assertEqual(Rating.objects.all().count(), 2)
+        UserProfile.objects.filter(user=user2.user).delete()
+        self.assertEqual(Rating.objects.all().count(), 0)
+
+        # Test a rating with negative value
         rating2 = Rating(user=user, article=article, rating=neg_val)
         self.assertRaises(ValidationError, lambda: rating2.full_clean())
 
-        # test a rating with zero value
+        # Test a rating with zero value
         rating3 = Rating(user=user, article=article, rating=zero_val)
         self.assertRaises(ValidationError, lambda: rating3.full_clean())
 
-        # test a rating with value larger than max value
+        # Test a rating with value larger than max value
         rating4 = Rating(user=user, article=article, rating=large_val)
         self.assertRaises(ValidationError, lambda: rating4.full_clean())
 
@@ -141,7 +197,7 @@ class ModelTests(TestCase):
         student_levels = UserLevelField.student_levels
         staff_levels = UserLevelField.staff_levels
 
-        # test correct number of levels
+        # Test correct number of levels
         self.assertEqual(len(student_levels), 6)
         self.assertEqual(len(staff_levels), 5)
 
@@ -149,7 +205,7 @@ class ModelTests(TestCase):
         user_pf.user.groups.add(Group.objects.get(name="student"))
         user_pf.level = staff_levels[0]
 
-        # test raise error assign staff level to student account
+        # Test raise error when assigning a staff level to a student account
         self.assertRaises(ValidationError, lambda: user_pf.save())
 
         # reset
@@ -159,7 +215,7 @@ class ModelTests(TestCase):
         user_pf.user.groups.add(Group.objects.get(name="staff"))
         user_pf.level = student_levels[0]
 
-        # test raise error assign student level to staff account
+        # Test raise error when assigning a student level to a staff account
         self.assertRaises(ValidationError, lambda: user_pf.save())
 
         # reset
@@ -169,7 +225,7 @@ class ModelTests(TestCase):
         user_pf.user.groups.add(Group.objects.get(name="student"))
         user_pf.level = student_levels[0]
 
-        # test correctness of assigning student level to student account
+        # Test correctness of assigning student level to student account
         # no error raises
         self.assertTrue(user_pf.save() is None)
         # level is saved correctly
@@ -182,7 +238,7 @@ class ModelTests(TestCase):
         user_pf.user.groups.add(Group.objects.get(name="staff"))
         user_pf.level = staff_levels[0]
 
-        # test correctness of assigning staff level to staff account
+        # Test correctness of assigning staff level to staff account
         # no error raises
         self.assertTrue(user_pf.save() is None)
         # level is saved correctly
@@ -196,15 +252,16 @@ class ModelTests(TestCase):
         article_views = ArticleViews(article=article1, views=1)
         article_views.save()
 
-        # test correct number of ArticleViews models
+        # Test correct number of ArticleViews models
         self.assertEqual(ArticleViews.objects.all().count(), 1)
 
         article_views2 = ArticleViews(article=article2, views=3)
         article_views2.save()
-        # test correct number of ArticleViews models
+
+        # Test correct number of ArticleViews models
         self.assertEqual(ArticleViews.objects.all().count(), 2)
 
-        # test integrity
+        # Test integrity
         article_views3 = ArticleViews(article=article2, views=45)
         with transaction.atomic():
             self.assertRaises(IntegrityError, lambda: article_views3.save())
@@ -213,14 +270,13 @@ class ModelTests(TestCase):
         ArticleViews.objects.all().delete()
         self.assertEqual(ArticleViews.objects.all().count(), 0)
 
-        # test same article could have different views on different date
-        # and on same date different articles could have different views
+        # Test different articles could have different views on same date
         article_views = ArticleViews(article=article1, views=5)
         article_views.save()
         article_views4 = ArticleViews(article=article2, views=5)
         self.assertIsNone(article_views4.save())
 
-        # pass.
+        # Test same article could have different views on different date
         with freeze_time(lambda: datetime(2018, 2, 4)):
             article_views5 = ArticleViews(article=article1, views=5)
             self.assertIsNone(article_views5.save())
