216cd7da373bcf3eb7bc53fbc895d7f35e5a9e7e
Bo Anderson <2323889A@student.gla.ac.uk>
2018-03-28 17:41:26 +0100

    Clear comment and rating forms on submission and prevent duplicates.

--- tragicreviews/views.py
+++ tragicreviews/views.py
@@ -68,6 +68,8 @@ def article(request, article_id, category_name_slug):
                 review.user = user_profile
                 review.article = article_object
                 review.save()
+
+                return HttpResponseRedirect(request.path_info)
             else:
                 print(form.errors)
             
@@ -78,6 +80,8 @@ def article(request, article_id, category_name_slug):
                 comment.user = user_profile
                 comment.article = article_object
                 comment.save()
+
+                return HttpResponseRedirect(request.path_info)
             else:
                 print(form.errors)
             
